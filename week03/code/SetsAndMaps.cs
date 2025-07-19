cf7d2f0fa4438e003eeadf9fc97367742acdfda6
marcosencarnacion <enc24001@byui.edu>
2025-07-19 12:01:04 -0500

    Implemented the FindPairs function and ran test cases to ensure they pass ✅

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -21,8 +21,29 @@ public static class SetsAndMaps
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        HashSet<string> seen = new HashSet<string>();
+        List<string> result = new List<string>();
+
+        foreach (string word in words)
+        {
+            // Skip words like "aa"
+            if (word[0] == word[1])
+                continue;
+
+            // Reverse the word
+            string reversed = new string(new char[] { word[1], word[0] });
+
+            if (seen.Contains(reversed))
+            {
+                result.Add($"{reversed} & {word}");
+            }
+            else
+            {
+                seen.Add(word);
+            }
+
+        }
+        return result.ToArray();
     }
 
     /// <summary>
