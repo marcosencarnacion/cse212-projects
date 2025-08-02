ea1d3ae6524aa51c5f27380b98011065f4c044f6
marcosencarnacion <enc24001@byui.edu>
2025-08-02 17:24:49 -0500

    Implemented recursive + memorized solution for Problem 3: Climbing Stairs Developed a recursive function with memoization to efficiently compute the number of ways to climb a staircase of s steps, allowing 1, 2, or 3 steps at a time.

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -99,7 +99,10 @@ public static class Recursion
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
-        // Base Cases
+        if (remember == null)
+            remember = new Dictionary<int, decimal>();
+
+        //Base Cases
         if (s == 0)
             return 0;
         if (s == 1)
@@ -109,10 +112,12 @@ public static class Recursion
         if (s == 3)
             return 4;
 
-        // TODO Start Problem 3
+        if (remember.ContainsKey(s))
+            return remember[s];
 
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+        remember[s] = ways;
         return ways;
     }
 
