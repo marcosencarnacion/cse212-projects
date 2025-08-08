907d1f776789ad22741f580c692f9449eacee18d
marcosencarnacion <enc24001@byui.edu>
2025-08-08 16:46:04 -0500

    Implemented the GetHeight function in the Node class to get height of the BinarySearchTree.

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -46,6 +46,8 @@ public class Node
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight); // Replace this line with the correct return statement(s)
     }
 }
